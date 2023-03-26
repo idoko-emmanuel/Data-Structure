0cb1bfb49efb479f6feda0da1abeb25fbed0046d
Idoko Emmanuel <idokoemmanuel@gmail.com>
2023-03-26 10:28:33 +0100

    implemented insert at first and last node in doubly linked list

--- DataStructure/LinkedList/DoublyLinkedList.php
+++ DataStructure/LinkedList/DoublyLinkedList.php
@@ -2,12 +2,62 @@
 namespace DataStructure\LinkedList;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use DataStructure\LinkedList\listNode;
+use DataStructure\LinkedList\DoublyListNode;
 
 
 class DoublyLinkedList  
 {
+    private $_firstNode = NULL;
+    private $_lastNode = NULL;
+    private $_totalNode = 0;
 
+    /*Inserting at first the node
+    When we add a node at the front or head, we have to check whether the list is empty or not. If the list is empty, both the first and last node will point to the newly created node. However, if the list already has a head, then we have to do the following:
+
+    Create the new node.
+    Make the new node as the first node or head.
+    Assign the previous head or first node as the next, to follow the newly created first node.
+    Assign the previous first node's previous link to the new first node.*/
+
+    public function insertAtFirst(string $data = NULL) {
+        $newNode = new ListNode($data);
+        if ($this->_firstNode === NULL) {
+            $this->_firstNode = &$newNode;
+            $this->_lastNode = $newNode; 
+        } else {
+            $currentFirstNode = $this->_firstNode; 
+            $this->_firstNode = &$newNode; 
+            $newNode->next = $currentFirstNode; 
+            $currentFirstNode->prev = $newNode; 
+        }
+        $this->_totalNode++; 
+        return TRUE;
+    }
+
+    /*Inserting at the last node
+    Since we are now tracking the last node, it will be easier to insert a new node at the end. First, we need to check that the list is not empty. If it is empty, then the new node becomes both the first and last node. However, if the list already has a last node, then we have to do the following:
+
+    Create the new node.
+    Make the new node the last node.
+    Assign the previous last node as the previous link of the current last node.
+    Assign the previous last node's next link to the new last node's previous link.*/
+
+    public function insertAtLast(string $data = NULL) { 
+        $newNode = new ListNode($data);
+        if ($this->_firstNode === NULL) {
+            $this->_firstNode = &$newNode; 
+            $this->_lastNode = $newNode; 
+        } else {
+            $currentNode = $this->_lastNode; 
+            $currentNode->next = $newNode; 
+            $newNode->prev = $currentNode; 
+            $this->_lastNode = $newNode; 
+        }
+        $this->_totalNode++; 
+        return TRUE;
+    }
+
+    
 }
 
 ?>
--- DataStructure/LinkedList/DoublyListNode.php
+++ DataStructure/LinkedList/DoublyListNode.php
@@ -4,6 +4,11 @@ namespace DataStructure\LinkedList;
 
 class DoublyListNode  
 {
+    /*Implementing a doubly linked list in PHP
+    We already know from the definition of a doubly linked list that a doubly linked list node will have two links: one to point to the next node and another to point to the previous node. Also, when we add a new node or delete a new node, we need to set both the next and previous references for each affected nodes. We saw a different approach in the singly linked list implementation where we did not track the last node, and as a result, we had to use an iterator to reach the last node each time. This time, we will track the last node, along with our insert and delete operations, to make sure our insert, and delete, and end operations have O(1) complexity.
+
+    Here is how the new node class will look with two link pointers followed by our barebones structure of a doubly linked list class:*/
+    
     public $data = NULL; 
     public $next = NULL; 
     public $prev = NULL; 
